afaaa55ecab15a12f02d7ac1380b2cc5211a5f63
Mike S <mpschendel@gmail.com>
2018-02-02 08:10:54 -0500

    Added openshift template, had php pull env vars for the database

--- telemetry_settings.php
+++ telemetry_settings.php
@@ -6,10 +6,10 @@ $db_type="mysql"; //Type of db: "mysql", "sqlite" or "postgresql"
 $Sqlite_db_file = "../telemetry.sql";
 
 // Mysql settings
-$MySql_username="USERNAME";
-$MySql_password="PASSWORD";
-$MySql_hostname="DB_HOSTNAME";
-$MySql_databasename="DB_NAME";
+$MySql_username= .$ENV["MYSQL_USERNAME"];
+$MySql_password=.$ENV["MYSQL_PASSWORD"];
+$MySql_hostname=.$ENV["MYSQL_SERVICE_NAME"];
+$MySql_databasename=.$ENV["MYSQL_DATABASE_NAME"];
 
 // Postgresql settings
 $PostgreSql_username="USERNAME";
